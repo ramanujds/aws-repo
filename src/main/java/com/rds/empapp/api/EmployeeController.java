6c7950e8da408c8a46d5f35b0677c1fac5d76154
Ramanuj Das <ramanujdas@Ramanujs-MacBook-Pro.local>
2023-08-30 15:42:53 +0530

    added files

---
+++ src/main/java/com/rds/empapp/api/EmployeeController.java
@@ -0,0 +1,42 @@
+package com.rds.empapp.api;
+
+import com.rds.empapp.model.Employee;
+import com.rds.empapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/employees")
+    public Employee saveEmployee(Employee employee){
+        return employeeService.saveEmployee(employee);
+    }
+
+    @PutMapping
+    public Employee updateEmployee(Employee employee){
+        return employeeService.updateEmployee(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployeeById(@PathVariable int id){
+        employeeService.deleteEmployeeById(id);
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable int id){
+        return employeeService.getEmployeeById(id);
+    }
+
+
+}
