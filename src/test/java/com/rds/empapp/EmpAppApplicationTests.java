209e6004a60cc7be2ba62eebc82d87af8a262a8b
Ramanuj Das <ramanujdas@Ramanujs-MacBook-Pro.local>
2023-08-30 16:40:28 +0530

    added files

--- src/test/java/com/rds/empapp/EmpAppApplicationTests.java
+++ src/test/java/com/rds/empapp/EmpAppApplicationTests.java
@@ -3,11 +3,11 @@ package com.rds.empapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class EmpAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
